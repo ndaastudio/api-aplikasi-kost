bfa15f8a42bb01e8416f6758636f28d0ea46880a
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2023-11-14 01:43:22 +0700

    feat: login user with token validation

--- app/Http/Controllers/auth/LoginController.php
+++ app/Http/Controllers/auth/LoginController.php
@@ -18,25 +18,62 @@ class LoginController extends Controller
 
     public function authenticate(Login $request)
     {
+        $isAvailableUpdate = version_compare($request->version, env('APP_VERSION'), '<');
+
+        if ($isAvailableUpdate) {
+            return response()->json([
+                'status' => false,
+                'message' => [
+                    'update' => 'Aplikasi telah tersedia dalam versi terbaru, silahkan update aplikasi Anda!',
+                ],
+                'data' => [
+                    'update_url' => env('APP_UPDATE_URL'),
+                ],
+            ]);
+        }
+
         $user = new User();
-        $isAuthenticate = $user->login($request->username, $request->password);
+        $isAuthenticate = $user->login($request->all());
 
         if ($isAuthenticate) {
-            $token = $isAuthenticate->createToken('authToken')->plainTextToken;
+            $userToken = $isAuthenticate->tokens()->where('name', $isAuthenticate->username)->first();
+
+            if (!$userToken) {
+                $userToken = $isAuthenticate->createToken($isAuthenticate->username)->plainTextToken;
+            } else if ($userToken && $request->konfirmasi_login == 1) {
+                $isAuthenticate->tokens()->where('name', $isAuthenticate->username)->delete();
+                $userToken = $isAuthenticate->createToken($isAuthenticate->username)->plainTextToken;
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => [
+                        'confirm' => 'Akun anda sedang digunakan pada perangkat lain. Apakah anda yakin ingin login di perangkat ini?',
+                    ]
+                ]);
+            }
 
             return response()->json([
                 'status' => true,
-                'message' => 'Berhasil login',
+                'message' => [
+                    'success' => 'Berhasil login!',
+                ],
                 'data' => [
                     'user' => $isAuthenticate,
-                    'token' => $token,
+                    'token' => $userToken,
+                ]
+            ]);
+        } else {
+            return response()->json([
+                'status' => false,
+                'message' => [
+                    'error' => 'Kombinasi username dan password yang Anda masukkan salah!',
                 ],
-            ], 200);
+            ]);
         }
 
         return response()->json([
             'status' => false,
-            'message' => 'Username atau password salah',
-        ], 401);
+            'message' => 'Terjadi kesalahan pada database atau server',
+        ], 500);
     }
 }
