f3c1a20f0f39ddbc36fa96ce3681eb31062f6bd7
Aprimivi Manda <35801535+ndaastudio@users.noreply.github.com>
2024-01-02 01:25:08 +0700

    feat: implement soft deletes to all models

--- app/Models/FasilitasKamar.php
+++ app/Models/FasilitasKamar.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FasilitasKamar extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'fasilitas_kamar';
 
--- app/Models/Kamar.php
+++ app/Models/Kamar.php
@@ -6,10 +6,11 @@ use App\Models\Order;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Kamar extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'kamar';
 
